9d9fe0aaaf5a53f0f893f3d7d30b4c3379d48380
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-10-12 01:58:14 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,22 +12,22 @@ class PostsController < ApplicationController
 	end
 
 	# app/controllers/posts_controller.rb
- 
+
 def create
   @post = Post.new(post_params(:title, :description))
   @post.save
   redirect_to post_path(@post)
 end
- 
+
 def update
   @post = Post.find(params[:id])
   @post.update(post_params(:title))
   redirect_to post_path(@post)
 end
- 
+
 private
- 
- 
+
+
 # We pass the permitted fields in as *args;
 # this keeps `post_params` pretty dry while
 # still allowing slightly different behavior
